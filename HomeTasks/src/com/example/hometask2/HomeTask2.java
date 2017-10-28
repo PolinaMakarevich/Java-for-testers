8029cc514a8ccdeaae1c5dc3c83324f344e0daf7
Polina Makarevich <Polina.Makarevich@t-systems.com>
2017-10-28 23:32:25 +0300

    Add task 2

---
+++ HomeTasks/src/com/example/hometask2/HomeTask2.java
@@ -0,0 +1,42 @@
+package com.example.hometask2;
+
+public class HomeTask2 {
+
+    public static void main(String[] args) {
+
+        byte[][] matrixA;
+        matrixA = new byte[10][10];
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                boolean flag = true;
+                while (flag) {
+                    flag = false;
+                    byte p = (byte) (Math.random() * 10);
+                    for (int q = 0; q < i; q++) {
+                        if (p == matrixA[q][j]) {
+                            flag = true;
+                        }
+                    }
+
+                    for (int f = 0; f < j; f++) {
+                        if (p == matrixA[i][f]) {
+                            flag = true;
+                        }
+                    }
+                    matrixA[i][j] = p;
+
+                }
+            }
+
+        }
+        for (int i = 0; i < 5; i++, System.out.println()) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print(matrixA[i][j] + " ");
+            }
+        }
+
+    }
+}
+
+
