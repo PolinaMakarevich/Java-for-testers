5e3d1fdc9bdf4673564dedcc1651e4069883bf9f
Polina Makarevich <Polina.Makarevich@t-systems.com>
2017-10-29 14:54:28 +0300

    added Home task 3

---
+++ HomeTasks/src/com/example/hometask3/HomeTask3.java
@@ -0,0 +1,43 @@
+package com.example.hometask3;
+
+public class HomeTask3 {
+    public static void main(String[] args) {
+
+        String password = "Qwerty123";
+        int strLength = password.length();
+        char[] chArray = password.toCharArray();
+
+        if ((strLength < 8) || (!isUpper(chArray)) || (!isLower(chArray)) || (!isNumber(chArray))) {
+            System.out.println("Password is not crypto-resistant");
+        } else {
+            System.out.println("Password is crypto-resistant");
+        }
+    }
+
+    public static boolean isUpper(char[] a) {
+        for (int i = 0; i < a.length; i++) {
+            if (Character.isUpperCase(a[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isLower(char[] a) {
+        for (int i = 0; i < a.length; i++) {
+            if (Character.isLowerCase(a[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isNumber(char[] a) {
+        for (int i = 0; i < a.length; i++) {
+            if (Character.isDigit(a[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
